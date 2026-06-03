b2248d790aaed836d996567fa4149917e37d633f
Pranoy Mukherjee <mukhep3@mcmaster.ca>
2026-06-02 23:11:09 -0400

    task 2

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ import Database from 'better-sqlite3';
 const app = express();
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,OPTIONS');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
+});
 
 app.get('/', (req, res) => {
   return res.status(200).send({'message': 'SHIPTIVITY API. Read documentation to see API docs'});
@@ -51,7 +60,7 @@ const validateId = (id) => {
  * @param {any} priority
  */
 const validatePriority = (priority) => {
-  if (Number.isNaN(priority)) {
+  if (Number.isNaN(priority) || priority < 1) {
     return {
       valid: false,
       messageObj: {
@@ -62,8 +71,37 @@ const validatePriority = (priority) => {
   }
   return {
     valid: true,
+  };
+};
+
+const validateStatus = (status) => {
+  if (status !== 'backlog' && status !== 'in-progress' && status !== 'complete') {
+    return {
+      valid: false,
+      messageObj: {
+        'message': 'Invalid status provided.',
+        'long_message': 'Status can only be one of the following: [backlog | in-progress | complete].',
+      },
+    };
   }
-}
+  return {
+    valid: true,
+  };
+};
+
+const sortByPriority = (clients) => clients.slice().sort((a, b) => a.priority - b.priority);
+
+const updateLanePriorities = (laneClients) => laneClients.map((laneClient, index) => ({
+  ...laneClient,
+  priority: index + 1,
+}));
+
+const persistClients = db.transaction((clientsToPersist) => {
+  const statement = db.prepare('update clients set status = ?, priority = ? where id = ?');
+  clientsToPersist.forEach((client) => {
+    statement.run(client.status, client.priority, client.id);
+  });
+});
 
 /**
  * Get all of the clients. Optional filter 'status'
@@ -118,14 +156,70 @@ app.put('/api/v1/clients/:id', (req, res) => {
   const id = parseInt(req.params.id , 10);
   const { valid, messageObj } = validateId(id);
   if (!valid) {
-    res.status(400).send(messageObj);
+    return res.status(400).send(messageObj);
   }
 
   let { status, priority } = req.body;
+  if (status !== undefined) {
+    const statusValidation = validateStatus(status);
+    if (!statusValidation.valid) {
+      return res.status(400).send(statusValidation.messageObj);
+    }
+  }
+
+  if (priority !== undefined) {
+    priority = parseInt(priority, 10);
+    const priorityValidation = validatePriority(priority);
+    if (!priorityValidation.valid) {
+      return res.status(400).send(priorityValidation.messageObj);
+    }
+  }
+
   let clients = db.prepare('select * from clients').all();
   const client = clients.find(client => client.id === id);
+  const nextStatus = status || client.status;
+  const isLaneChange = nextStatus !== client.status;
+  const isPriorityChange = priority !== undefined && priority !== client.priority;
 
   /* ---------- Update code below ----------*/
+  if (!isLaneChange && !isPriorityChange) {
+    return res.status(200).send(clients);
+  }
+
+  const sourceLane = sortByPriority(
+    clients.filter((laneClient) => laneClient.status === client.status && laneClient.id !== client.id),
+  );
+  const targetLaneBase = isLaneChange
+    ? sortByPriority(clients.filter((laneClient) => laneClient.status === nextStatus))
+    : sourceLane;
+
+  let targetPriority = priority;
+  if (targetPriority === undefined) {
+    targetPriority = isLaneChange ? targetLaneBase.length + 1 : client.priority;
+  }
+
+  if (targetPriority > targetLaneBase.length + 1) {
+    targetPriority = targetLaneBase.length + 1;
+  }
+
+  const movedClient = {
+    ...client,
+    status: nextStatus,
+    priority: targetPriority,
+  };
+
+  const targetLane = targetLaneBase.slice();
+  targetLane.splice(targetPriority - 1, 0, movedClient);
+
+  const updatedSourceLane = updateLanePriorities(sourceLane);
+  const updatedTargetLane = updateLanePriorities(targetLane);
+  const clientsToPersist = isLaneChange
+    ? updatedSourceLane.concat(updatedTargetLane)
+    : updatedTargetLane;
+
+  persistClients(clientsToPersist);
+
+  clients = db.prepare('select * from clients').all();
 
 
 
